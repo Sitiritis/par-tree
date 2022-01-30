c94d01d4406224a0703657d0fa95d58f269c2550
Tymur Lysenko <nanotimcool@gmail.com>
2022-01-30 17:59:50 +0300

    Add functions to AST

--- src/Core/Ast.hs
+++ src/Core/Ast.hs
@@ -4,29 +4,55 @@ module Core.Ast (
 ) where
 
 import qualified Data.Text as T
-import Prettyprinter (Doc, Pretty (pretty), align, nest, softline, unsafeViaShow, (<+>))
-import Prelude (Integer, Show, (.), (<>))
+import Prettyprinter (Doc, Pretty (pretty), align, hardline, nest, parens, sep, softline, unsafeViaShow, vsep, (<+>))
+import Relude (Foldable (toList), Integer, NonEmpty ((:|)), Show, toList, (.), (<$>), (<>))
 
 -- TODO: refine
 newtype Identifier = Identifier T.Text deriving (Show)
 
 data Expr
   = Lit Integer
-  | Var Identifier
+  | Val Identifier
   | Let Identifier Expr
   | Add Expr Expr
   | Mult Expr Expr
+  | Func Identifier (NonEmpty Identifier) (NonEmpty Expr)
+  | App Identifier (NonEmpty Expr)
   deriving (Show)
 
-softWrappedBinOp :: (Pretty l, Pretty r) => Doc a -> l -> r -> Doc a
-softWrappedBinOp op l r = pretty l <+> op <> softline <> (align . pretty) r
+prettyWithParens :: Expr -> Doc ann
+prettyWithParens (Lit i) = pretty i
+prettyWithParens (Val i) = pretty i
+prettyWithParens e = parens (pretty e)
+
+softWrappedBinOp :: Doc a -> Doc a -> Doc a -> Doc a
+softWrappedBinOp op l r = l <+> op <> softline <> align r
 
 instance Pretty Identifier where
   pretty (Identifier name) = pretty name
 
 instance Pretty Expr where
   pretty (Lit i) = unsafeViaShow i
-  pretty (Var name) = pretty name
+  pretty (Val name) = pretty name
   pretty (Let name e) = "let" <+> pretty name <+> "=" <> nest 2 (softline <> pretty e)
-  pretty (Add el er) = softWrappedBinOp "+" el er
-  pretty (Mult el er) = softWrappedBinOp "*" el er
+  pretty (Add el er) = softWrappedBinOp "+" (prettyWithParens el) (prettyWithParens er)
+  pretty (Mult el er) = softWrappedBinOp "*" (prettyWithParens el) (prettyWithParens er)
+  pretty (Func name args (expr :| [])) =
+    pretty name <+> prettyArgs <+> "="
+      <> nest 2 (softline <> prettyBody)
+   where
+    argDocs = pretty <$> args
+    prettyArgs = (align . sep . toList) argDocs
+    prettyBody = pretty expr
+  pretty (Func name args body) =
+    pretty name <+> prettyArgs <+> "="
+      <> nest 2 (hardline <> prettyBody)
+   where
+    argDocs = pretty <$> args
+    prettyArgs = (align . sep . toList) argDocs
+    bodyDocs = pretty <$> body
+    prettyBody = (vsep . toList) bodyDocs
+  pretty (App name args) = sep prettyAppWithArgs
+   where
+    argDocs = prettyWithParens <$> args
+    prettyAppWithArgs = pretty name : toList argDocs
--- src/Examples.hs
+++ src/Examples.hs
@@ -3,7 +3,7 @@ module Examples where
 import Core.Ast (Expr (..), Identifier (..))
 import Prettyprinter (Pretty (pretty))
 import Prettyprinter.Util (putDocW)
-import Prelude (IO, Int, putStrLn, (*>))
+import Relude (IO, Int, NonEmpty ((:|)), putStrLn, (*>))
 
 two :: Expr
 two = Lit 2
@@ -20,6 +20,40 @@ simpleProd = Mult two seven
 
 letExample :: Expr
 letExample = Let (Identifier "x") simpleProd
+
+addFunc :: Expr
+addFunc =
+  Func
+    (Identifier "add")
+    ( Identifier "x"
+        :| [Identifier "y"]
+    )
+    (Add (Val (Identifier "x")) (Val (Identifier "y")) :| [])
+
+functionExample :: Expr
+functionExample =
+  Func
+    (Identifier "func")
+    ( Identifier "first"
+        :| [ Identifier "second"
+           , Identifier "third"
+           ]
+    )
+    ( addFunc
+        :| [ Let
+              (Identifier "y")
+              ( App
+                  (Identifier "add")
+                  ( Mult (Val (Identifier "first")) (Val (Identifier "second"))
+                      :| [Lit 4]
+                  )
+              )
+           , Let
+              (Identifier "z")
+              (Mult (Val (Identifier "y")) (Lit 4))
+           , Add (Add (Val (Identifier "z")) (Val (Identifier "third"))) (Lit 1)
+           ]
+    )
 
 exprppW :: Int -> Expr -> IO ()
 exprppW w e = putDocW w (pretty e) *> putStrLn ""
